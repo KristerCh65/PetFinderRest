a297e6f419952de33904933b531ead3b624ea36a
KristerCh <KBMChavez@outlook.com>
2020-11-13 00:17:51 -0600

    create models 13-11-20

--- PetFinderApi/Models/Person.cs
+++ PetFinderApi/Models/Person.cs
@@ -1,11 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace PetFinderApi.Models
 {
     public class Person
     {
+        [Key]
+        public long idPerson { get; set; } 
+
+        [Required]
+        [Column(TypeName ="varchar(15)")]
+        public string Identification { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
     }
 }
---
+++ PetFinderApi/Models/Pet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetFinderApi.Models
+{
+    public class Pet
+    {
+        [Key]
+        public long idPet { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string NamePet { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(20)")]
+        public string Specie { get; set; }
+
+        [Column(TypeName = ("varchar(80)"))]
+        public string Race { get; set; }
+
+        public DateTime Age { get; set; }
+
+        public DateTime RescueDate { get; set; }
+
+        public string Size { get; set; }
+
+        public string Photo { get; set; }
+    }
+}
